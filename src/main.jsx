91472b70ec51ff86f22cd46ebf1e84b8dc62aee0
Marouane JEBBAR Wakama Founder <marouane.jebbar@wakama.farm>
2025-07-26 15:17:35 +0000

    Remove local Tailwind/PostCSS config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import ChatPage from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
